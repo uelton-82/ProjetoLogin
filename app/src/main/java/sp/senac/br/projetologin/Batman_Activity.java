faab0cd78b2f2c134c8b46f77b6b3c9921bca412
UELTON SANTOS DA SILVA <uelton.ssilva@senacsp.edu.br>
2019-05-23 08:56:34 -0300

    Initial commit

---
+++ app/src/main/java/sp/senac/br/projetologin/Batman_Activity.java
@@ -0,0 +1,13 @@
+package sp.senac.br.projetologin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Batman_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_batman_);
+    }
+}
